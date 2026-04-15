989930be1f99d2a7358d99c2c55cde61f87e2127
ztc64003 <1294811773@qq.com>
2026-04-15 09:16:21 +0800

    chore: bind local server to localhost

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import uvicorn
 from app.server import create_app
 
 if __name__ == "__main__":
-    uvicorn.run(create_app, factory=True, host="0.0.0.0", port=8000)
+    uvicorn.run(create_app, factory=True, host="127.0.0.1", port=8000)
